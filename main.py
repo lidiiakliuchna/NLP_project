70875031b103433d9350032f16a611666cbb58e6
lidiiakliuchna <lida.kluchnaya@gmail.com>
2024-03-17 19:58:46 +0000

    cleaning the sentences

--- main.py
+++ main.py
@@ -27,3 +27,21 @@ language_german = 'deu'
 english_sentences = extract_language_sentences(file_name, language_english)
 russian_sentences = extract_language_sentences(file_name, language_russian)
 german_sentences = extract_language_sentences(file_name, language_german)
+
+def clean_text(text):
+    # Remove special characters
+    cleaned_text = re.sub(r'\xa0', ' ', text)
+    # Remove punctuation
+    cleaned_text = re.sub(r'[^\w\s]', '', cleaned_text)
+    # Convert to lowercase
+    cleaned_text = cleaned_text.lower()
+    # Remove spaces
+    cleaned_text = re.sub(r'\s+', '', cleaned_text).strip()
+    return cleaned_text
+
+
+# Clean each sentence
+cleaned_russian_sentences = [clean_text(sentence) for sentence in russian_sentences]
+cleaned_english_sentences = [clean_text(sentence) for sentence in english_sentences]
+cleaned_german_sentences = [clean_text(sentence) for sentence in german_sentences]
+
