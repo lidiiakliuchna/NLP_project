fdc5f1ae5632769e4c7ddbae87be795c69749cdf
lidiiakliuchna <lida.kluchnaya@gmail.com>
2024-03-17 22:55:12 +0000

    distribution of frequencies across these n-grams

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from string import punctuation
 from sklearn.metrics import classification_report, confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 def extract_language_sentences(file_name, language):
@@ -99,6 +101,82 @@ for lang, lang_sentences in zip(['Russian', 'English', 'German'], [cleaned_russi
     for text in lang_sentences:
         true_labels.append(lang)
         predicted_labels.append(predict_language(text, lang2char_ngrams))
+
+# Print classification report
+print(classification_report(true_labels, predicted_labels))
+
+# Print confusion matrix
+labels = list(set(true_labels))
+print(confusion_matrix(true_labels, predicted_labels, labels=labels))
+
+# Plot confusion matrix heatmap
+fig, ax = plt.subplots(figsize=(10, 5))
+sns.heatmap(data=confusion_matrix(true_labels, predicted_labels, labels=labels),
+            annot=True, fmt="d", xticklabels=labels, yticklabels=labels, ax=ax)
+plt.title("Confusion matrix")
+plt.show()
+
+# Create a common vocabulary of n-grams
+vocab = Counter()
+
+# Update the vocabulary with n-grams 
+for lang_sentences in [cleaned_russian_sentences, cleaned_english_sentences, cleaned_german_sentences]:
+    for text in lang_sentences:
+        vocab.update(n_grams(text.lower(), 2))
+
+# Select top 5000 n-grams
+vocab = [word for word, _ in vocab.most_common(5000)]
+
+# Create n-gram to index and index to n-gram dictionaries
+id2ngram = {i: ngram for i, ngram in enumerate(vocab)}
+ngram2id = {ngram: i for i, ngram in enumerate(vocab)}
+
+# Represent each language as a vector of n-gram frequencies
+lang2vec = defaultdict(lambda: np.zeros((len(vocab))))
+
+
+for lang, lang_sentences in zip(['Russian', 'English', 'German'], [cleaned_russian_sentences, cleaned_english_sentences, cleaned_german_sentences]):
+    for text in lang_sentences:
+        ngrams = n_grams(text.lower(), 2)
+        for ngram in ngrams:
+            if ngram in ngram2id:
+                lang2vec[lang][ngram2id[ngram]] += 1
+
+
+# Predict language based on cosine similarity
+def predicted_language(text, lang2vec):
+    table = str.maketrans({ch: None for ch in punctuation})
+    text_ngrams = n_grams(text.lower().translate(table), 2)
+    text_vec = np.zeros((len(vocab)))
+
+    
+    for ngram in text_ngrams:
+        if ngram in ngram2id:
+            text_vec[ngram2id[ngram]] += 1
+
+    lang2sim = {}
+
+    # Compute cosine similarity between input text vector and language vectors
+    for lang, lang_vec in lang2vec.items():
+        similarity = cosine_similarity([text_vec], [lang_vec])[0][0]
+        lang2sim[lang] = similarity
+
+    # Predict the language with the highest similarity
+    return max(lang2sim, key=lambda x: lang2sim[x])
+
+
+# Test the predict_language function
+predicted_lang_1 = predicted_language('Das ist fantastisch ', lang2vec)
+print(predicted_lang_1)
+
+# Predict languages for all texts
+true_labels = []
+predicted_labels = []
+
+for lang, lang_sentences in zip(['Russian', 'English', 'German'], [cleaned_russian_sentences, cleaned_english_sentences, cleaned_german_sentences]):
+    for text in lang_sentences:
+        true_labels.append(lang)
+        predicted_labels.append(predicted_language(text, lang2vec))
 
 # Print classification report
 print(classification_report(true_labels, predicted_labels))
